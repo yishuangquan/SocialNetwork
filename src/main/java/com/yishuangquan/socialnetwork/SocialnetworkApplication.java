cb21631b07f22c3bd25a804ac8e33cf43ab6cbea
yishuangquan <yishuangquan@aliyun.com>
2018-03-08 14:47:27 +0800

    建立空项目

---
+++ src/main/java/com/yishuangquan/socialnetwork/SocialnetworkApplication.java
@@ -0,0 +1,12 @@
+package com.yishuangquan.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialnetworkApplication.class, args);
+	}
+}
